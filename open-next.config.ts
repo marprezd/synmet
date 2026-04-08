e25218f2d6526b92c2580c3b6f393cd6e5a5108d
marprezd <marprezd@gmail.com>
2026-04-08 13:51:24 -0500

    fix(Cloudflare R2): :bug: No R2 binding NEXT_INC_CACHE_R2_BUCKET found. homemarprezd.cursor-serverbin0f8217a84adf66daf250228a3ebf0da631d3c9b0/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration-bash.sh

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,6 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
+import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue'
 
 export default defineCloudflareConfig({
   // Uncomment to enable R2 cache,
@@ -7,4 +8,7 @@ export default defineCloudflareConfig({
   // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
   // See https://opennext.js.org/cloudflare/caching for more details
   incrementalCache: r2IncrementalCache,
+  queue: doQueue,
+  // Disable this if you want to use PPR
+  enableCacheInterception: true,
 })
